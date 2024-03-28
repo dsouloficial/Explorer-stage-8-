fd99315ab659ea9988726c58286fe055fac454ce
Denilson Bernardo <denilsonbnascimento07@gmail.com>
2024-03-27 22:24:09 -0300

    finish stage8

---
+++ src/utils/AppError.js
@@ -0,0 +1,11 @@
+class AppError {
+  message;
+  statusCode;
+
+  constructor(message, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = AppError;
